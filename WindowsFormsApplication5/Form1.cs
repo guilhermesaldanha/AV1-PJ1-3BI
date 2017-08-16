edba68e94d456f472a16197f3429deb0f7d4d928
guilhermesaldanha <saldanha.guilherme1902@gmail.com>
2017-08-16 11:48:25 -0300

    AV1 Concluida
    Guilherme Saldanha, solo AV1_PJ1_3BI

---
+++ WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication5
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int batata = rnd.Next(0, trackBar1.Value);
+
+            label1.Text = batata.ToString();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+        }
+    }
+}
